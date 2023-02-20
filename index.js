c071ea98ba0d3b5cb56564f42d3a18d7371cb9c4
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-02-20 19:55:57 +0100

    style header

--- index.js
+++ index.js
@@ -97,3 +97,12 @@ function toogleLightMode() {
   var element = document.body;
   element.classList.toggle("light-mode");
 }
+function myFunction() {
+  let checkBox = document.getElementById("myCheck");
+  let text = document.getElementById("text");
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
