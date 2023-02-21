af485b5b641ea9b209b87fc089f6a6c34b833523
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-02-21 15:10:19 +0100

    JS function to copy pasword one or two

--- index.js
+++ index.js
@@ -99,6 +99,10 @@ let inputElTwo = document.getElementById("input-el-2");
 console.log(inputElTwo.textContent);
 let generatedPasswordOne = generateRandomPassword();
 let generatedPasswordTwo = generateRandomPassword();
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
 
 function getRandomCharacter() {
   let randomChar = Math.floor(Math.random() * characters.length);
@@ -136,23 +140,231 @@ console.log(
 );
 function copyPasswordOne() {
   inputElOne.select();
-  document.execCommand("copy");
+  navigator.clipboard.writeText(inputElOne.value);
   inputElOne.value = "Copied!";
 }
+
 function copyPasswordTwo() {
   inputElTwo.select();
-  document.execCommand("copy");
+  navigator.clipboard.writeText(inputElTwo.value);
   inputElTwo.value = "Copied!";
 }
-// Function to exclude symbols
-function excludeSymbols() {
-  characters.splice(62, 32);
+// Function to include all characters
+function includeAllCharacters() {
+  characters.splice(
+    0,
+    94,
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "[",
+    "}",
+    "]",
+    ",",
+    "|",
+    ":",
+    ";",
+    "<",
+    ">",
+    ".",
+    "?",
+    "/"
+  );
   console.log(characters);
   newRandomPassword();
 }
-// Function to exclude numbers
-function excludeNumbers() {
-  characters.splice(52, 10);
+
+// Function to include only symbols
+function includeOnlySymbols() {
+  characters.splice(
+    0,
+    94,
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "[",
+    "}",
+    "]",
+    ",",
+    "|",
+    ":",
+    ";",
+    "<",
+    ">",
+    ".",
+    "?",
+    "/"
+  );
+  console.log(characters);
+  newRandomPassword();
+}
+
+// Function to include only numbers
+
+function includeOnlyNumbers() {
+  characters.splice(0, 94, "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  console.log(characters);
+  newRandomPassword();
+}
+
+// Function to include only lower case letters
+function includeOnlyLowerLetters() {
+  characters.splice(
+    0,
+    94,
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  );
+  console.log(characters);
+  newRandomPassword();
+}
+
+// Function to include only upper case letters
+function includeOnlyUpperLetters() {
+  characters.splice(
+    0,
+    94,
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  );
   console.log(characters);
   newRandomPassword();
 }
